f40354ca0e63b83f1e4918eaec7faf06ddf7a0dc
Vladislav <perezzzq@yandex.ru>
2023-08-14 15:08:04 +0300

    basic game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "./components/Button";
+import { Game } from "./components/Game";
 
-function App() {
+import "./App.less";
+
+const App = () => {
+  const [date, setDate] = useState<Date>(new Date());
+
+  const handleRestart = () => {
+    setDate(new Date());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="header">
+          <div>
+            <h1>Play 2048</h1>
+          </div>
+          <div>
+            <Button onClick={handleRestart}>Restart</Button>
+          </div>
+        </div>
+        <Game key={date.toISOString()} />
+      </div>
   );
-}
+};
 
-export default App;
+
+export default App
---
+++ src/components/Board/models/Board.ts
@@ -0,0 +1,8 @@
+import { pixelSize } from "../../../styles";
+
+
+export const tileCount = 4;
+
+export const animationDuration = 250;
+
+export const boardMargin = 2 * pixelSize;
